c4a178009e38813ca68c8ad9d3f4b66ee193fbbe
Aradhana Ramamoorthy <159184994+AradhanaRamamoorthy@users.noreply.github.com>
2024-11-18 16:04:13 -0500

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,5 +1,10 @@
 //import postDataFunctions from './posts.js';
+import { createMood, getAllMoods, getMoodByName, getMoodById } from "./moods.js";
+import { createInterest } from "./interest.js";
+
 import userDataFunctions from './Users.js';
 
 export const userData = userDataFunctions;
+export const moodsData = {createMood, getAllMoods, getMoodByName, getMoodById};
+export const interestData = {createInterest};
 //export const postData = postDataFunctions;

b2c7e1d773f05c82f39a4fb2d32d0c8b985d2c90
Aradhana Ramamoorthy <159184994+AradhanaRamamoorthy@users.noreply.github.com>
2024-11-18 16:08:53 -0500

    Update index.js
    Importing the MoodRoutes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import userRoutes from './users.js';
 import authRoutes from './auth.js';
+import moodsRoute from './moods.js';
 
 import {static as staticDir} from 'express';
 const constructorMethod = (app) => {
   app.use('/', userRoutes);
   app.use('/auth', authRoutes);
+  app.use('/', moodsRoute);
   app.use('/public', staticDir('public'));
   app.use('*', (req, res) => {
     res.redirect('/');

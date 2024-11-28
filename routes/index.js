aa73ec2f2c4e12d2d5164efbb076ba64bb01f416
Aradhana Ramamoorthy <159184994+AradhanaRamamoorthy@users.noreply.github.com>
2024-11-28 11:48:35 -0500

    Update index.js
    Updating the routes of place

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import userRoutes from './users.js';
 import authRoutes from './auth.js';
 import moodsRoute from './moods.js';
 import interestsRoute from './interests.js';  
+import placesRoute from './places.js';
 
 import { static as staticDir } from 'express';
 
@@ -10,6 +11,7 @@ const constructorMethod = (app) => {
   app.use('/auth', authRoutes); 
   app.use('/', moodsRoute);  
   app.use('/interests', interestsRoute);  
+  app.use('/',placesRoute);
   app.use('/public', staticDir('public'));  
   app.use('*', (req, res) => {
     res.redirect('/');  

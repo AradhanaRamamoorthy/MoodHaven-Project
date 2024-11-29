db3fe401d30ad26cf5da61957d713b1842b8b54f
Aradhana <aara1999@gmail.com>
2024-11-29 12:18:13 -0500

    Modified the placepage to handle more errors appropriately

--- routes/places.js
+++ routes/places.js
@@ -1,34 +1,57 @@
 import {Router} from 'express';
 const router = Router();
 import { placesData } from '../data/index.js';
+import helpers from '../helpers.js';
 
 
 router.post('/location', async (req, res) => {
     const { latitude, longitude, activity } = req.body;
-    
-    if (!latitude || !longitude) {
-      return res.status(400).json({ error: 'Invalid location data.' });
+    if(!latitude || !longitude) {
+      res.status(400).json({ error: 'Invalid location data.' });
     }
-    if (!activity) {
-      return res.status(400).json({ error: 'Activity is required.' });
+    if(!activity) {
+      res.status(400).json({ error: 'Activity is required.' });
     }
     try {
       res.status(200).json({ message: 'Location and activity received successfully' });
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred while processing location.' });
+    } catch (e) {
+      res.status(500).json({ error: e });
     }
   });
 
   router.get('/placepage/:activity', async (req, res) => {
-    const activity = req.params.activity;
-    if (!activity) {
-      return res.status(400).send('Activity is required.');
+    let selected_Activity = req.params.activity;
+    try //New
+    {
+      selected_Activity = helpers.checkString(selected_Activity);
     }
-    try {
-       const places = await placesData.getPlacesByActivities(activity);
-       res.render('users/placepage', {places: places , layout : 'mainMood'});
-    } catch (error) {
-      res.status(500).send('Cannot load the page.');
+    catch(e)
+    {
+      return res.status(400).render('users/interests',
+        {
+        title : 'Interest Page',
+        errors: 'You must select an activity!',
+        hasErrors: true,
+        selected_activity : selected_Activity
+        });
+    } 
+    try
+    {
+        const places = await placesData.getPlacesByActivities(selected_Activity);
+        if(!places || places.length === 0){
+        return res.status(404).render('users/placepage', {
+            title : 'Place Page',
+            errors: 'Place not found!',
+            hasErrors: true,
+          });
+        }
+    return res.status(200).render('users/placepage', {
+         places: places , 
+         layout : 'mainMood'
+      });
+    } 
+    catch(e) {
+      res.status(500).json({error : e});
     }
   });
 

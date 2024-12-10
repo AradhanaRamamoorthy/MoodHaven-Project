6a04721cecf236b3d33c8797e4f06cb7b727d1ca
Aradhana <aara1999@gmail.com>
2024-12-10 06:22:26 -0500

    Updated the route to fetch the comments and dynamically disply in the Place Page and fixed some error handling on the router.

--- routes/places.js
+++ routes/places.js
@@ -1,8 +1,8 @@
 import {Router} from 'express';
 const router = Router();
 import { placesData } from '../data/index.js';
-import { users } from '../config/mongoCollections.js';
 import userDataFunctions from '../data/Users.js';
+import { users } from '../config/mongoCollections.js';
 import helpers from '../helpers.js';
 import { ObjectId } from 'mongodb';
 
@@ -17,9 +17,7 @@ router.post('/location', async (req, res) => {
     const { latitude, longitude, activity } = req.body;
     const userId = req.session.user?._id;
     if (!userId) {
-      console.log('User ID not found in session.');
-    } else {
-      console.log('User ID:', userId);
+      res.status(400).json({ error: 'UserId is not found' });
     }
     if(!latitude || !longitude) {
       res.status(400).json({ error: 'Invalid location data.' });
@@ -39,19 +37,12 @@ router
 .route('/placepage/:activity')
 .get(isAuthenticated, async (req, res) => {
     const activity = req.params.activity;
-    const userId = req.session.user?._id;
+    const userId = req.session.user._id;
     if (!activity) {
       return res.status(400).send('Activity is required.');
     }
-    if (!userId) {
-      console.log('User ID not found in session.');
-    } else {
-      console.log('User ID:', userId);
-    }
-
     try {
        const places = await placesData.getPlacesByActivities(activity);
-       if (userId) {
         const userCollection = await users();
         await userCollection.updateOne(
             { _id: new ObjectId(userId) },
@@ -59,11 +50,61 @@ router
               $set: { searched: true, searchedPlaces: places }, 
             }
         );
-      }
-       res.render('users/placepage', {places: places , layout : 'mainMood'});
+        const user_details = await userDataFunctions.getUserById(userId);
+        const user_name = `${user_details.firstName} ${user_details.lastName}`;
+        if(!places || places.length === 0){
+          return res.status(404).render('users/placepage', {
+              title : 'Place Page',
+              errors: 'Place not found!',
+              hasErrors: true,
+            });
+          }
+          for (let place of places) {
+            let place_data = await placesData.getCommentsByPlaceId(place._id.toString());
+           place.comments = place_data || [];
+        }
+        res.render('users/placepage', {
+          title : 'Places Page',
+          places: places.map(place => ({ ...place, user_name })) , 
+          layout : 'places'
+        });
+      
     } catch (error) {
       console.error('Error updating user or fetching places:', error);
       res.status(500).send('Cannot load the page.');
+    }
+  });
+
+
+  router.post('/placepage/:place_Id/comments', async(req,res) => {
+    let place_Id = req.params.place_Id;
+    let { comment_Text, user_name } = req.body; 
+    comment_Text = comment_Text.trim();
+    user_name = user_name.trim();
+    if (!comment_Text || typeof comment_Text !== 'string' || comment_Text.length === 0 || !user_name || typeof user_name !== 'string' || user_name.length === 0) {
+      return res.status(400).send('Comment text and user name are required.');
+    }
+
+    try
+    {
+      place_Id = helpers.checkId(place_Id);
+      comment_Text = helpers.checkString(comment_Text, "user_comment");
+      const place = await placesData.getPlaceById(place_Id);
+      if(!place)
+      {
+        return res.status(404).json({error : "Place not found!"});
+      }
+      const comment_added = {
+        comment_content: comment_Text,
+        comment_author: user_name,
+        date: new Date().toISOString()
+      };
+      const updated_place = await placesData.user_comments(place_Id, comment_added);
+      res.status(200).json(updated_place.comment);
+    }
+    catch(e)
+    {
+      res.status(500).json({error : "Failed to add comment!"});
     }
   });
 
